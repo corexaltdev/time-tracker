5e05b00a7e5706a93d1e5d20ce2964cf3d099e5d
corexaltdev <ravihidayatinisia@gmail.com>
2024-06-04 04:45:12 +0700

    feat: Work with password stuffs and routing

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('page.login');
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +15,62 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/dashboard', function () {
+    return view('page.dashboard');
+});
+
+Route::get('/ad-manage-account', function () {
+    return view('page.admin.ad-manage-account');
+});
+
+Route::get('/cl-view-project', function () {
+    return view('page.client.cl-view-project');
+});
+
+Route::get('/dev-tasks', function () {
+    return view('page.developer.dev-tasks');
+});
+
+Route::get('/dev-update-tasks', function () {
+    return view('page.developer.dev-update-tasks');
+});
+
+Route::get('/dev-modify-info', function () {
+    return view('page.developer.dev-modify-info');
+});
+
+Route::get('/man-clients', function () {
+    return view('page.manager.man-clients');
+});
+
+Route::get('/man-create-client', function () {
+    return view('page.manager.man-create-client');
+});
+
+Route::get('/man-create-task', function () {
+    return view('page.manager.man-create-task');
+});
+
+Route::get('/man-edit-client', function () {
+    return view('page.manager.man-edit-client');
+});
+
+Route::get('/man-edit-task', function () {
+    return view('page.manager.man-edit-task');
+});
+
+Route::get('/man-projects', function () {
+    return view('page.manager.man-projects');
+});
+
+Route::get('/man-tasks', function () {
+    return view('page.manager.man-tasks');
+});
+
+Route::get('/man-update-project', function () {
+    return view('page.manager.man-update-project');
 });
 
 require __DIR__.'/auth.php';
